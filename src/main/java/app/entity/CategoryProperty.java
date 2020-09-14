8996a520837f4725bbd9cb2688fc4297151e7e64
user <user@a.com>
2020-09-14 22:56:06 +0200

    Changed CategoryProperty

--- src/main/java/app/entity/CategoryProperty.java
+++ src/main/java/app/entity/CategoryProperty.java
@@ -3,10 +3,7 @@ package app.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -16,5 +13,7 @@ public class CategoryProperty
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private int id;
+    @ManyToOne
+    private Category category;
     private String name;
 }
